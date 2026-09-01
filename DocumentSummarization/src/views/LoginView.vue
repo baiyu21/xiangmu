2be6ee4e631c6c92b8b60ff6b430b028d2f647cc
HE343483 <heshuaiheng3@gmail.com>
2026-09-01 16:51:29 +0800

    页面更改

--- DocumentSummarization/src/views/LoginView.vue
+++ DocumentSummarization/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, computed, watch } from 'vue'
+import { ref, reactive, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { ElMessage } from 'element-plus'
 
