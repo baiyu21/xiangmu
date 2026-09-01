39554e1f4c8407dc05df4b8de65b7c3c94cd856b
HE343483 <heshuaiheng3@gmail.com>
2026-09-01 15:32:09 +0800

    框架

---
+++ DocumentSummarization/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleLogin = () => {
+  router.push({ name: 'login' })
+}
+
+const handleRegister = () => {
+  router.push({ name: 'register' })
+}
+</script>
+
+<template>
+  <div>
+    <h1>登录</h1>
+    <el-button type="primary" @click="handleLogin">登录</el-button>
+    <el-button type="primary" @click="handleRegister">注册</el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
